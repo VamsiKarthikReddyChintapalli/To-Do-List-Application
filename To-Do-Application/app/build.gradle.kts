2845bc6311c63e48373fad28a9dc5a5fd5598fdf
VamsiKarthikReddyChintapalli <20A91A0517@aec.edu.in>
2024-01-10 11:22:57 +0530

    Initial commit

---
+++ To-Do-Application/app/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.to_do_application"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.to_do_application"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+//    implementation ("fileTree(dir: 'libs', include: ['*.jar'])")
+//
+//    implementation ("androidx.appcompat:appcompat:1.2.0")
+//
+//    implementation ("com.google.android.material:material:1.2.1")
+//
+//    implementation ("androidx.constraintlayout:constraintlayout:2.0.1")
+//
+//    implementation ("androidx.recyclerview: recyclerview:1.1.0")
+//
+//    implementation ("androidx.cardview:cardview:1.0.0")
+
+    testImplementation ("junit:junit:4.12")
+
+    androidTestImplementation ("androidx.test.ext:junit:1.1.2")
+
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.3.0")
+
+//    implementation ("it.xabaras.android:recyclerview-swipedecorator:1.2.3")
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+//    implementation ("com.google.android.material:material:1.2.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("it.xabaras.android:recyclerview-swipedecorator:1.4")
+}
---
+++ To-Do-Application/app/src/main/java/com/example/to_do_application/OnDialogCloseListner.java
@@ -0,0 +1,8 @@
+package com.example.to_do_application;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListner {
+
+    void onDialogClose(DialogInterface dialogInterface);
+}
